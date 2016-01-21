86cd7634f1d33dcfc1f3ebeeaab8b8ecf8afb455
aaaddress1 <aaaddress1@gmail.com>
2016-01-21 22:24:19 +0800

    no message

---
+++ myOCR/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace myOCR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.orgPictureBox = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.labelAnser1 = new System.Windows.Forms.Label();
+            this.labelAnser2 = new System.Windows.Forms.Label();
+            this.labelAnser3 = new System.Windows.Forms.Label();
+            this.labelAnser4 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orgPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(260, 51);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 157);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OCR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // orgPictureBox
+            // 
+            this.orgPictureBox.Location = new System.Drawing.Point(12, 69);
+            this.orgPictureBox.Name = "orgPictureBox";
+            this.orgPictureBox.Size = new System.Drawing.Size(260, 49);
+            this.orgPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.orgPictureBox.TabIndex = 2;
+            this.orgPictureBox.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(278, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 168);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // labelAnser1
+            // 
+            this.labelAnser1.AutoSize = true;
+            this.labelAnser1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelAnser1.Location = new System.Drawing.Point(98, 130);
+            this.labelAnser1.Name = "labelAnser1";
+            this.labelAnser1.Size = new System.Drawing.Size(20, 24);
+            this.labelAnser1.TabIndex = 4;
+            this.labelAnser1.Text = "?";
+            // 
+            // labelAnser2
+            // 
+            this.labelAnser2.AutoSize = true;
+            this.labelAnser2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelAnser2.Location = new System.Drawing.Point(124, 130);
+            this.labelAnser2.Name = "labelAnser2";
+            this.labelAnser2.Size = new System.Drawing.Size(20, 24);
+            this.labelAnser2.TabIndex = 5;
+            this.labelAnser2.Text = "?";
+            // 
+            // labelAnser3
+            // 
+            this.labelAnser3.AutoSize = true;
+            this.labelAnser3.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelAnser3.Location = new System.Drawing.Point(150, 130);
+            this.labelAnser3.Name = "labelAnser3";
+            this.labelAnser3.Size = new System.Drawing.Size(20, 24);
+            this.labelAnser3.TabIndex = 6;
+            this.labelAnser3.Text = "?";
+            // 
+            // labelAnser4
+            // 
+            this.labelAnser4.AutoSize = true;
+            this.labelAnser4.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labelAnser4.Location = new System.Drawing.Point(176, 130);
+            this.labelAnser4.Name = "labelAnser4";
+            this.labelAnser4.Size = new System.Drawing.Size(20, 24);
+            this.labelAnser4.TabIndex = 7;
+            this.labelAnser4.Text = "?";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.result.Location = new System.Drawing.Point(12, 130);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(80, 24);
+            this.result.TabIndex = 8;
+            this.result.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 188);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.labelAnser4);
+            this.Controls.Add(this.labelAnser3);
+            this.Controls.Add(this.labelAnser2);
+            this.Controls.Add(this.labelAnser1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.orgPictureBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ISU Recaptcha OCR By.aaaddress1.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.orgPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox orgPictureBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label labelAnser1;
+        private System.Windows.Forms.Label labelAnser2;
+        private System.Windows.Forms.Label labelAnser3;
+        private System.Windows.Forms.Label labelAnser4;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ myOCR/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+namespace myOCR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //cannot find 'K','O', '0'
+        Dictionary<int, char> direct = new Dictionary<int, char>()
+        {
+           {28146,'1'}, {47557,'2'},{44616,'3'},{45190,'4'},{43216,'5'},{42926,'6'},{26290,'7'},{76136,'8'},{40420,'9'},
+            {39744,'A'},{43226,'B'},{46524,'C'},{46534,'D'},{48984,'E'},{21222,'F'},{47224,'G'},
+            {36570,'H'},{23148,'I'},{45360,'J'},{66378,'L'},{47320,'M'},{77168,'N'},{26518,'P'},
+            {47472,'Q'},{38960,'R'},{43316,'S'},{8332,'T'},{57234,'U'},{61924,'V'},{95880,'W'},
+            {72664,'X'},{43640,'Y'},{48398,'Z'}
+        };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             this.openFileDialog.ShowDialog();
+             if (openFileDialog.FileName == "") return;
+             richTextBox1.Clear();
+
+             Image currPic = Image.FromFile(openFileDialog.FileName);
+             Bitmap currBm = new Bitmap(openFileDialog.FileName);
+             orgPictureBox.Image = Image.FromFile(openFileDialog.FileName); ;
+         
+            Thread n = (new Thread(() => {
+                bool lastBlack = false;
+                int hashVal = 0;
+                int pos = 0;
+                int lastX = 0;
+                pictureBox.Image = (new Bitmap(currPic));
+                Graphics tmpG = Graphics.FromImage(pictureBox.Image);
+                for (int i = 0; i < currPic.Size.Width-1; i++)
+                {
+                    this.Invoke(new Action(() => { tmpG.DrawLine(Pens.Red, new Point(i, 0), new Point(i, currPic.Size.Height)); pictureBox.Image = (new Bitmap(currPic)); }));
+                    this.Refresh();
+                    bool blackCheck = false;
+                    int currYLineHash = 0;
+                    for (int y = 0,lastY = 0; y < currPic.Size.Height; y++)
+                    {
+                        if ((currBm.GetPixel(i, y).ToArgb()) != -1)
+                        {
+                            if (!blackCheck) this.Invoke(new Action(() => { Graphics.FromImage(currPic).DrawLine(Pens.Blue, new Point(i, 0), new Point(i, currPic.Size.Height)); }));
+                            blackCheck = true;
+                            currYLineHash += y * ((lastY - y < 1 ? y - lastY : lastY - y) * (i - lastX));
+                            lastY = y;
+                        }
+                        else if (blackCheck)//發現黑色後遇到y
+                        {
+                            currYLineHash += (i-lastX);
+                        }
+                       
+                    }
+                    
+
+                    if (lastBlack && (!blackCheck))
+                    {
+                        pos++;
+                        string tmpName = "labelAnser" + pos.ToString();
+                       
+                        if (direct.ContainsKey(hashVal))
+                        {
+                            (this.Controls.Find(tmpName, true)[0] as Label).Text = "" + direct[hashVal];
+                            this.Invoke(new Action(() => { this.richTextBox1.Text += "end at X = " + i + "\n第" + pos + "個字符 = " + direct[hashVal] + "\n\n"; }));
+                        }
+                        else
+                        {
+                            (this.Controls.Find(tmpName, true)[0] as Label).Text = "?";
+                            this.Invoke(new Action(() => { this.richTextBox1.Text += "end at X = " + i + "\n第" + pos + "個字符Hash = {" + hashVal + "\n\n"; }));
+                            //this.Invoke(new Action(() => { this.richTextBox1.Text +="{" + hashVal + ",''}\n\n"; }));
+                       
+                        }
+                    
+                    }
+                    else if ((!lastBlack) && (blackCheck))
+                    {
+                        hashVal = 0;
+                        lastX = i;
+                        this.Invoke(new Action(() => { this.richTextBox1.Text += "start at X = " + i + "\n"; }));
+                    }else if (blackCheck&&blackCheck)
+                    {
+                        hashVal += currYLineHash;
+                    }
+                    lastBlack = (blackCheck);
+                  
+                }
+
+            }));
+
+            n.IsBackground = true;
+            n.Start();
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            Form.CheckForIllegalCrossThreadCalls = false;
+        }
+    }
+}
